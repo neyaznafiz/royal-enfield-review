12a1ae327f227a9545ddb54709f5c7e87d537030
niiyazobug <niiyaz5555@gmail.com>
2022-04-04 00:23:18 +0600

    dike img and text added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,17 +3,17 @@ import CoustomLink from '../CoustomLink/CoustomLink';
 
 const Header = () => {
     return (
-        <nav className='bg-gray-300'>
+        <nav className=''>
 
-         
-           <ul className='flex space-x-4 py-4 justify-center'>
+
+            <ul className='flex space-x-4 py-7 justify-center font-semibold'>
                 <CoustomLink to='/' className="border-b-2">HOME</CoustomLink>
                 <CoustomLink to='/reviews' className="border-b-2">REVIEWS</CoustomLink>
                 <CoustomLink to='/dashboard' className="border-b-2">DASHBOARD</CoustomLink>
                 <CoustomLink to='/blogs' className="border-b-2">BLOGS</CoustomLink>
                 <CoustomLink to='/about' className="border-b-2">ABOUT</CoustomLink>
             </ul>
-          
+
         </nav>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2 className="text-4xl">home</h2>
+
+        <div className='grid grid-cols-2 justify-items-center items-center'>
+            <div className=''>
+                <h2 className='text-6xl font-bold py-5'>Royal Enfield</h2>
+                <h2 className='text-6xl font-bold'>Things to Know</h2>
+                <p className='p-6'>I’ve read the Royal Enfield Himalayan’s manual and paperwork so you don’t have to. Here’s the most important things I learnt about my new motorcycle. First things to do with your new beast Set your clock. Hold select and mode buttons for 3 seconds then press it to set hours. Hold select for 3 seconds.</p>
+            </div>
+
+            <div className=''>
+                <img className='my-6 rounded-md' src="royal-enfield.png" alt="" />
+            </div>
+
+        </div>
+        
         </div>
     );
 };
