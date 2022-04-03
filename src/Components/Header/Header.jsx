7625f3a60ca6d6196f055c59531f1925b66a7e20
niiyazobug <niiyaz5555@gmail.com>
2022-04-03 22:19:48 +0600

    header added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,14 @@ import CoustomLink from '../CoustomLink/CoustomLink';
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='bg-gray-300'>
             
+<CoustomLink to='/'>HOME</CoustomLink>
+<CoustomLink to='/reviews'>REVIEWS</CoustomLink>
+<CoustomLink to='/dashboard'>DASHBOARD</CoustomLink>
+<CoustomLink to='/blogs'>BLOGS</CoustomLink>
+<CoustomLink to='/about'>ABOUT</CoustomLink>
+
         </nav>
     );
 };
