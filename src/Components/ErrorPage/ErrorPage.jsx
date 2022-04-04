fd8b643a897b6280d3278250f47056465ccbd007
niiyazobug <niiyaz5555@gmail.com>
2022-04-04 15:25:48 +0600

    error page edited

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BsArrowBarLeft } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+
 
 const ErrorPage = () => {
+
+const navigate = useNavigate()
+
     return (
-        <div>
-            <h2>404 not found</h2>
+        <div className=''>
+            <img className='rounded-lg mx-auto' src="error.gif" alt="" />
+
+            <button className='flex mx-auto bg-sky-200 my-5 px-12 py-2 font-bold rounded-lg text-gray-600 hover:shadow-lg hover:bg-gray-400 hover:text-white'  onClick={()=> navigate(`/`)}>
+                <BsArrowBarLeft className='my-auto mr-2 text-2xl font-bold'></BsArrowBarLeft> GO TO HOME
+            </button>
         </div>
     );
 };
--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -6,31 +6,31 @@ const Reviews = () => {
 
     const [reviews] = useReviewHooks()
 
-    // const { picture, name, comment, rating } = reviews
-
     return (
         <div>
 
             {
-                reviews.map(review => <div className=''>
+                reviews.map(review =>
 
-                    <div className='shadow-xl px-10 pb-5 pt-2 mx-10 mt-6 rounded-md'>
+                    <div className=''>
 
-                        <div className='flex  gap-3'>
-                            <img className='w-10 h-10 rounded-full ' src={review.picture} alt="" />
+                        <div className='shadow-xl px-10 pb-5 pt-2 mx-10 mt-6 rounded-md'>
 
-                            <div>
-                                <h1 className='font-bold pt-1 whitespace-nowrap text-lg'>{review.name}</h1>
+                            <div className='flex  gap-3'>
+                                <img className='w-10 h-10 rounded-full ' src={review.picture} alt="" />
 
-                                <p className='whitespace-nowrap flex font-semibold'> {review.rating} <BsStarHalf className='text-xs text-yellow-400 my-1.5 '></BsStarHalf></p>
+                                <div>
+                                    <h1 className='font-bold pt-1 whitespace-nowrap text-lg'>{review.name}</h1>
+
+                                    <p className='whitespace-nowrap flex font-semibold'> {review.rating} <BsStarHalf className='text-xs text-yellow-400 my-1.5 '></BsStarHalf></p>
+                                </div>
+
+                                <p className='pt-2'>{review.comment}</p>
                             </div>
 
-                            <p className='pt-2'>{review.comment}</p>
                         </div>
 
-                    </div>
-
-                </div>)
+                    </div>)
             }
 
         </div>
